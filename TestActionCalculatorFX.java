01a79882e28d1a6c43a81bb36ddb6c37e67bb531
WenChi <u10416019@go.utaipei.edu.tw>
2016-04-16 01:14:01 +0800

    Main class of calculator
    This file is the main class of the ActionCalculatorFX, and it just call the ActionCalculatorFX class then action it.

---
+++ TestActionCalculatorFX.java
@@ -0,0 +1,10 @@
+//U10416019鄭文琪
+
+import javafx.application.Application;
+
+public class TestActionCalculatorFX{
+	public static void main(String[] args){
+		//call the ActionCalculatorFX class
+		Application.launch(ActionCalculatorFX.class, args);
+	}
+}
